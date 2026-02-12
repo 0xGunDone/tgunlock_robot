7324f6013716b04090560cc3158e067ac7743da8
0xGunDone <125687774+0xGunDone@users.noreply.github.com>
2026-02-12 22:06:17 +0300

    upd

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import uvicorn
+
+
+if __name__ == "__main__":
+    host = os.getenv("APP_HOST", "127.0.0.1")
+    port = int(os.getenv("APP_PORT", "8051"))
+    uvicorn.run("app.main:app", host=host, port=port)
